55c8224968e588d7289270802f47ed393c206e59
pranadeepika <119491225+pranadeepika@users.noreply.github.com>
2023-11-28 18:52:11 +0530

    Delete department-service directory

--- department-service/src/main/java/spring/departmentservice/service/DepartmentService.java
+++
@@ -1,9 +0,0 @@
-package spring.departmentservice.service;
-
-import spring.departmentservice.dto.DepartmentDto;
-
-public interface DepartmentService {
-    DepartmentDto saveDepartment(DepartmentDto departmentDto);
-
-    DepartmentDto getDepartmentByCode(String code);
-}
