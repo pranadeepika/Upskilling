298e32a24cb0c63df2a1e9bee26b3121fff8a517
pranadeepika <119491225+pranadeepika@users.noreply.github.com>
2023-11-29 10:23:32 +0530

    Add files via upload

---
+++ springboot-kafka/src/main/java/spring/springbootkafka/controller/JsonMessageController.java
@@ -0,0 +1,28 @@
+package spring.springbootkafka.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import spring.springbootkafka.kafka.JsonKafkaProducer;
+import spring.springbootkafka.payload.User;
+
+@RestController
+@RequestMapping("jsonkafka")
+public class JsonMessageController {
+	
+	private JsonKafkaProducer kafkaProducer;
+
+    public JsonMessageController(JsonKafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user){
+        kafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json message sent to kafka topic");
+    }
+
+}
